f38fca60d3bce89f890d22d5761137817a06a888
Ghadaj <ghada.aljurbua@gmail.com>
2021-02-27 01:33:24 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,10 +1,8 @@
-import os
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "agency"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
 
@@ -19,58 +17,24 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
-'''
-Movies
-'''
-class Movies(db.Model):  
-  __tablename__ = 'movies'
-
-  id = Column(Integer, primary_key=True)
-  title = Column(String)
-  release_date = Column(String)
-
-  def __init__(self, title, release_date):
-    self.titel = title
-    self.release_date = releas_date
-
-
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
-
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
-
-  def format(self):
-    return {
-      'id': self.id,
-      'title': self.title,
-      'release_date': self.release_date,
-    }
 
 '''
-Actors
+Person
+Have title and release year
 '''
-class Actors(db.Model):  
-  __tablename__ = 'actors'
+class Person(db.Model):  
+  __tablename__ = 'People'
 
   id = Column(Integer, primary_key=True)
   name = Column(String)
-  age = Column(Integer)
-  gender = Column(String)
-  def __init__(self, name,age,gender):
+  catchphrase = Column(String)
+
+  def __init__(self, name, catchphrase=""):
     self.name = name
-    self.age = age
-    self.gender = gender
+    self.catchphrase = catchphrase
 
   def format(self):
     return {
       'id': self.id,
-      'name': self.name
-      'age': self.age
-      'gender': self.gender
-    }
+      'name': self.name,
+      'catchphrase': self.catchphrase}
