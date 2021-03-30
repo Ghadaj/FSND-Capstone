70076291fce883db4e0a6e94576a8d3e9d3db343
Ghadaj <ghada.aljurbua@gmail.com>
2021-03-30 19:04:53 +0300

    commit

--- auth.py
+++ auth.py
@@ -1,10 +1,12 @@
+import os
 import json
 from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+
 AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
-ALGORITHMS = os.environ.get['ALGORITHMS']
+ALGORITHMS = os.environ.get('ALGORITHMS')
 API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 
